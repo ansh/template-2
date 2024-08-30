24003fd7c1329423de8699d4eb00dec0e1d6b523
Ansh Nanda <anshnanda10@gmail.com>
2024-08-30 09:27:00 -0700

    create deepgram & microphone helpers for the template

---
+++ src/contexts/DeepgramContext.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import {
+  createClient,
+  LiveClient,
+  SOCKET_STATES,
+  LiveTranscriptionEvents,
+  type LiveSchema,
+  type LiveTranscriptionEvent,
+} from "@deepgram/sdk";
+
+import { createContext, useContext, useState, ReactNode, FunctionComponent } from "react";
+
+interface DeepgramContextType {
+  connection: LiveClient | null;
+  connectToDeepgram: (options: LiveSchema, endpoint?: string) => Promise<void>;
+  disconnectFromDeepgram: () => void;
+  connectionState: SOCKET_STATES;
+}
+
+const DeepgramContext = createContext<DeepgramContextType | undefined>(undefined);
+
+interface DeepgramContextProviderProps {
+  children: ReactNode;
+}
+
+const getApiKey = async (): Promise<string> => {
+  const response = await fetch("/api/authenticate", { cache: "no-store" });
+  const result = await response.json();
+  return result.key;
+};
+
+const DeepgramContextProvider: FunctionComponent<DeepgramContextProviderProps> = ({ children }) => {
+  const [connection, setConnection] = useState<LiveClient | null>(null);
+  const [connectionState, setConnectionState] = useState<SOCKET_STATES>(SOCKET_STATES.closed);
+
+  /**
+   * Connects to the Deepgram speech recognition service and sets up a live transcription session.
+   *
+   * @param options - The configuration options for the live transcription session.
+   * @param endpoint - The optional endpoint URL for the Deepgram service.
+   * @returns A Promise that resolves when the connection is established.
+   */
+  const connectToDeepgram = async (options: LiveSchema, endpoint?: string) => {
+    const key = await getApiKey();
+    const deepgram = createClient(key);
+
+    const conn = deepgram.listen.live(options, endpoint);
+
+    conn.addListener(LiveTranscriptionEvents.Open, () => {
+      setConnectionState(SOCKET_STATES.open);
+    });
+
+    conn.addListener(LiveTranscriptionEvents.Close, () => {
+      setConnectionState(SOCKET_STATES.closed);
+    });
+
+    setConnection(conn);
+  };
+
+  const disconnectFromDeepgram = async () => {
+    if (connection) {
+      connection.finish();
+      setConnection(null);
+    }
+  };
+
+  return (
+    <DeepgramContext.Provider
+      value={{
+        connection,
+        connectToDeepgram,
+        disconnectFromDeepgram,
+        connectionState,
+      }}
+    >
+      {children}
+    </DeepgramContext.Provider>
+  );
+};
+
+// Use the useDeepgram hook to access the deepgram context and use the deepgram in any component.
+// This allows you to connect to the deepgram and disconnect from the deepgram via a socket.
+// Make sure to wrap your application in a DeepgramContextProvider to use the deepgram.
+function useDeepgram(): DeepgramContextType {
+  const context = useContext(DeepgramContext);
+  if (context === undefined) {
+    throw new Error("useDeepgram must be used within a DeepgramContextProvider");
+  }
+  return context;
+}
+
+export {
+  DeepgramContextProvider,
+  useDeepgram,
+  SOCKET_STATES,
+  LiveTranscriptionEvents,
+  type LiveTranscriptionEvent,
+};

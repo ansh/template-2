2f051369a9f787b359d499c3dd82af210e11cf25
carlvellotti <carlvellotti@gmail.com>
2025-02-15 17:50:35 -0800

    remove google auth

--- src/lib/hooks/useAuth.ts
+++ src/lib/hooks/useAuth.ts
@@ -1,4 +1,12 @@
+'use client';
+
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
-export const useAuth = () => useContext(AuthContext);
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+}

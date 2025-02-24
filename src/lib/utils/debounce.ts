cbe7ae87096f1d4c85981350dfd835f2033744f1
carlvellotti <carlvellotti@gmail.com>
2025-02-24 13:19:38 -0800

    Add custom debounce utility and update imports

---
+++ src/lib/utils/debounce.ts
@@ -0,0 +1,10 @@
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout;
+  return (...args: Parameters<T>) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), wait);
+  };
+} 

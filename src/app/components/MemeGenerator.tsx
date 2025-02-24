8d66cf7cc6d3556fc42d63d788a095cc1488b1c4
carlvellotti <carlvellotti@gmail.com>
2025-02-24 13:10:53 -0800

    Fix: Update lodash import syntax

--- src/app/components/MemeGenerator.tsx
+++ src/app/components/MemeGenerator.tsx
@@ -9,7 +9,7 @@ import { toast } from 'react-hot-toast';
 import { createMemeVideo } from '@/lib/utils/videoProcessor';
 import { BackgroundImage, TextSettings } from '@/lib/types/meme';
 import { createMemePreview } from '@/lib/utils/previewGenerator';
-import debounce from 'lodash/debounce';
+import { debounce } from 'lodash';
 import ImagePicker from './ImagePicker';
 
 // Import or define the SelectedMeme interface
